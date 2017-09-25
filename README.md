8ad87a7af10c66b71048b8a5d09f0bfdfd689575
Casselman <208546@ent.nytint.com>
2017-09-25 18:35:04 -0400

    Improve documentation.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tidy_seasonal
 Wrapper for 'seasonal' package that's more friendly for tidy dataframes.
+This is a single-function package. 
+'seas_adj()' takes three arguments:
+df  A data frame containing a date-formatted variable containing dates and a numeric column containing the values to be adjusted.
+name_of_date_col  The bare (no quote marks) name of the date variable.
+name_of_value_col The bare (no quote marks) name of the values variable.
+
+This is my first R package and was created for my own purposes. It may not work for you! If it doesn't, I may not know how to fix it! But I welcome feedback: ben.casselman@nytimes.com, twitter: @bencasselman
--- tidyseasonal/R/seas_adj.R
+++ tidyseasonal/R/seas_adj.R
@@ -1,18 +1,23 @@
 #' Tidy Seasonal Adjustment
 #'
-#' Wrapper for 'seasonal' package.
-#' @param df A data frame
+#' Wrapper for 'seasonal' package that plays more nicely with tidy data.
+#' Input is a data frame (or similar object) that contains a date-formatted date variable and a numeric value variable.
+#' Output is a vector of length equal to nrow(df) containing seasonall-adjusted values.
+#' For now, output does *not* include any of the other outputs of the 'seas' function in the 'seasonal' package.
+#' @param df A data frame containing dates and values as separate variables.
 #' @param name_of_date_col Name of df column containing dates. Must be date formatted.
-#' @param name_of_val_col Name of df column containing values. Must be numeric.
+#' @param name_of_val_col Name of df column containing values to be seasonally adjusted.
 #' @keywords seasonal
 #' @export
 #' @examples 
 #' df$SA <- function(df, date, NSA)
 
-seas_adjust <- function(df, name_of_date_col = "date", name_of_val_col = "value") {
+seas_adjust <- function(df, name_of_date_col = date, name_of_val_col = value) {
+  
   library(seasonal)
   library(dplyr)
-  
+  library(lubridate)
+
   expr1 <- enquo(name_of_date_col)
   expr2 <- enquo(name_of_val_col)
   
